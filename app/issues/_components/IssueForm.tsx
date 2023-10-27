f182d9e0a4fc9a11d893c49de83f91a29dfb62af
jacksonkalmbach <jacksonrkalmbach@gmail.com>
2023-10-26 21:14:47 -0500

    add update route

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -35,7 +35,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setIsSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) {
+        await axios.patch("/api/issues/" + issue.id, data);
+      } else {
+        await axios.post("/api/issues", data);
+      }
       router.push("/issues");
     } catch (error) {
       setIsSubmitting(false);
@@ -69,7 +73,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Summit New Issue
+          {issue ? "Update Issue" : "Summit New Issue"}{" "}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
